68559599207054f6895d9bf1a3c7e2d89aaa490b
Игорь <55245070+RBVV23@users.noreply.github.com>
2022-04-15 22:55:14 +0300

    1504 2255

---
+++ matrix2vector.m
@@ -0,0 +1,12 @@
+function [ vector ] = matrix2vector( matrix )
+
+Nstr=size(matrix,1);
+Nstl=size(matrix,2);
+vector=zeros(1,Nstr*Nstl);
+
+for i=1:Nstr
+   vector( ((i-1)*Nstl+1) : i*Nstl )=matrix(i,:);
+end
+
+end
+
